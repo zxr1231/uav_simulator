299fcf3df40ecd7216e80f63d2f09166ab7d448a
zxr1231 <204028866+zxr1231@users.noreply.github.com>
2026-09-11 14:37:13 +0800

    build: keep Gazebo plugins outside source tree

--- gazeboSetup.bash
+++ gazeboSetup.bash
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+workspace_dir=$(cd -- "${dir}/../../.." && pwd)
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${dir}/models
 export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:${dir}/urdf
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:${dir}/plugins
+export GAZEBO_PLUGIN_PATH=${workspace_dir}/devel/lib:$GAZEBO_PLUGIN_PATH:${dir}/plugins
